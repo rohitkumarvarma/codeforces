42ed455aa2977171849ab176450146f2d6e2295b
rohitkumarvarma <53017078+rohitkumarvarma@users.noreply.github.com>
2019-09-26 10:20:23 +0530

    Add files via upload

---
+++ Dawid and Bags of Candies#1230A.py
@@ -0,0 +1,20 @@
+#Dawid has four bags of candies. The i-th of them contains ai candies.
+#Also, Dawid has two friends. He wants to give each bag to one of his two friends.
+#Is it possible to distribute the bags in such a way that each friend receives the same amount of candies in total?
+bags = []
+for i in range (4):
+    candies=int(input(" "))
+    boags.append(candies)
+
+bags[0]+bags[1] = a
+bags[0]+bags[2] = b
+bags[0]+bags[3] = c
+bags[1]+bags[2] = d
+bags[1]+bags[3] = e
+bags[2]+bags[3] = f
+if (a%2==0 or b%2==0 or c%2==0 or d%2==0 or e%2==0 f%2==0):
+    print("yes")
+else:
+    print("no")
+    
+    
